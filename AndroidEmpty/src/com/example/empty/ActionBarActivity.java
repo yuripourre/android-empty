7d9395adf22302a8f845595511af1b8288650300
Yuri Pourre <yuriamayuri@gmail.com>
2014-02-05 12:05:50 -0200

    Android Empty Project

---
+++ AndroidEmpty/src/com/example/empty/ActionBarActivity.java
@@ -0,0 +1,83 @@
+package com.example.empty;
+
+import com.example.empty.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
+
+public class ActionBarActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		LinearLayout layout = new LinearLayout(this.getBaseContext());
+
+		layout.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+		layout.setBackgroundColor(0xFF0000FF);
+
+		setContentView(layout);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		MenuItem item = addActionBarItem(menu, R.string.action_search, R.drawable.ic_action_search);
+		
+		MenuItem item2 = addActionBarItem(menu, "New", R.drawable.ic_action_new);
+				
+		item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				itemAction();
+				return true;
+			}
+			
+		});
+
+		return super.onCreateOptionsMenu(menu);
+		
+	}
+
+	private void itemAction(){
+		Toast.makeText(getApplicationContext(), "Item Action", Toast.LENGTH_SHORT).show();
+	}
+	
+	private int menuItems = 0;
+	
+	protected MenuItem addActionBarItem(Menu menu, String title, int iconId){
+		
+		menuItems++;
+		
+		MenuItem item = menu.add(Menu.NONE, menuItems, Menu.NONE, title);
+		
+		item.setIcon(iconId);
+		
+		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		
+		return item;
+		
+	}
+	
+	protected MenuItem addActionBarItem(Menu menu, int titleId, int iconId){
+		
+		menuItems++;
+		
+		MenuItem item = menu.add(Menu.NONE, menuItems, Menu.NONE, titleId);
+		
+		item.setIcon(iconId);
+		
+		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		
+		return item;
+		
+	}
+	
+}
